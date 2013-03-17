e6c486eb674740030cf17a6693ba7316c2c59399
RobbieClarken <robbie.clarken@gmail.com>
2013-03-18 01:08:04 +1100

    Added table view for editing logins

---
+++ AS Logins/EditLoginTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditLoginTableViewController.h
+//  AS Logins
+//
+//  Created by Robbie Clarken on 17/03/13.
+//  Copyright (c) 2013 Robbie Clarken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditLoginTableViewController : UITableViewController
+
+@end
---
+++ AS Logins/EditLoginTableViewController.m
@@ -0,0 +1,105 @@
+//
+//  EditLoginTableViewController.m
+//  AS Logins
+//
+//  Created by Robbie Clarken on 17/03/13.
+//  Copyright (c) 2013 Robbie Clarken. All rights reserved.
+//
+
+#import "EditLoginTableViewController.h"
+
+@interface EditLoginTableViewController ()
+
+@end
+
+@implementation EditLoginTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.tableView setEditing:YES animated:NO];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"EditableLoginCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    CGRect textFrame = cell.detailTextLabel.frame;
+    textFrame.size.width = cell.contentView.frame.size.width - textFrame.origin.x;
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFrame];
+    textField.font = cell.detailTextLabel.font;
+    textField.text = @"host";
+    [cell.contentView addSubview:textField];
+    cell.textLabel.text = @"Hostname";
+    cell.detailTextLabel.text = @"";
+    
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {  
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView
+           editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    return UITableViewCellEditingStyleNone;
+}
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
