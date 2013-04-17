b959f3fe8c5d2b06f671100972858c1bd31c0925
RobbieClarken <robbie.clarken@gmail.com>
2013-04-17 21:40:29 +1000

    Update devices view using fetched results controller

--- AS Logins/DevicesTableViewController.m
+++ AS Logins/DevicesTableViewController.m
@@ -11,10 +11,13 @@
 #import "DeviceCell.h"
 #import "Device+Create.h"
 
-static NSString *DevicesKey = @"devices";
+// TODO: Prevent device cell appearing before modal view is presented
+
 static NSString *DeviceCellIdentifier = @"DeviceCellIdentifier";
 
-@interface DevicesTableViewController () <DeviceViewControllerDelegate>
+@interface DevicesTableViewController () <DeviceViewControllerDelegate, NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
 
 @end
 
@@ -25,11 +28,34 @@ static NSString *DeviceCellIdentifier = @"DeviceCellIdentifier";
     self.title = @"Devices";
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(showNewDeviceViewController)];
     [self.tableView registerClass:[DeviceCell class] forCellReuseIdentifier:DeviceCellIdentifier];
+    self.fetchedResultsController.delegate = self;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self.tableView reloadData];
+}
+
+- (NSFetchedResultsController *)fetchedResultsController {
+    if (_fetchedResultsController) {
+        return _fetchedResultsController;
+    }
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"Device"];
+    fetchRequest.predicate = [NSPredicate predicateWithFormat:@"group == %@ AND toDelete == NO", self.group];
+    fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES]];
+    _fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.group.managedObjectContext sectionNameKeyPath:nil cacheName:nil];
+    // TODO: Handle error
+    [_fetchedResultsController performFetch:nil];
+    return _fetchedResultsController;
+}
+
+- (NSUInteger)numberOfDevices {
+    id <NSFetchedResultsSectionInfo> sectionInfo = self.fetchedResultsController.sections[0];
+    return sectionInfo.numberOfObjects;
+}
+
+- (Device *)deviceForIndexPath:(NSIndexPath *)indexPath {
+    return [self.fetchedResultsController objectAtIndexPath:indexPath];
 }
 
 - (void)showNewDeviceViewController {
@@ -44,6 +70,39 @@ static NSString *DeviceCellIdentifier = @"DeviceCellIdentifier";
     [self presentViewController:navigationController animated:YES completion:^{}];
 }
 
+#pragma mark - fetched results controller delegate
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView beginUpdates];
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView endUpdates];
+}
+
+- (void)controller:(NSFetchedResultsController *)controller
+   didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath
+     forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath {
+    UITableView *tableView = self.tableView;
+    switch(type) {
+        case NSFetchedResultsChangeInsert: {
+            [tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+        }
+        case NSFetchedResultsChangeDelete:
+            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+        case NSFetchedResultsChangeUpdate:
+            [tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+        case NSFetchedResultsChangeMove:
+            [tableView reloadRowsAtIndexPaths:@[indexPath, newIndexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+    }
+}
+
 #pragma mark - device view delegate
 
 - (void)deviceViewController:(DeviceViewController *)editDeviceViewController didFinishWithSave:(BOOL)save {
@@ -54,11 +113,8 @@ static NSString *DeviceCellIdentifier = @"DeviceCellIdentifier";
             NSLog(@"Unresolved error in %s: %@, %@", __PRETTY_FUNCTION__, error, [error userInfo]);
             abort();
         }
-        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:[self.group.devices count]-1 inSection:0];
-        [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
-        
-        Device *device = [self.group.devices lastObject];
-        
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:[self numberOfDevices]-1 inSection:0];
+        Device *device = [self deviceForIndexPath:indexPath];
         DeviceViewController *newDeviceViewController = [[DeviceViewController alloc] initWithStyle:UITableViewStyleGrouped];
         newDeviceViewController.device = device;
         [self.navigationController pushViewController:newDeviceViewController animated:NO];
@@ -71,12 +127,12 @@ static NSString *DeviceCellIdentifier = @"DeviceCellIdentifier";
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self.group.devices count];
+    return [self numberOfDevices];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     DeviceCell *cell = [tableView dequeueReusableCellWithIdentifier:DeviceCellIdentifier forIndexPath:indexPath];
-    Device *device = [self.group.devices objectAtIndex:indexPath.row];
+    Device *device = [self deviceForIndexPath:indexPath];
     cell.textLabel.text = device.name;
     cell.detailTextLabel.text = device.hostname;
     return cell;
@@ -84,21 +140,20 @@ static NSString *DeviceCellIdentifier = @"DeviceCellIdentifier";
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        NSMutableOrderedSet *devices = [self.group mutableOrderedSetValueForKey:DevicesKey];
-        [devices removeObjectAtIndex:indexPath.row];
+        Device *device = [self deviceForIndexPath:indexPath];
+        device.toDelete = @YES;
         NSError *error;
         [self.group.managedObjectContext save:&error];
         if (error) {
             NSLog(@"Unresolved error in %s: %@, %@", __PRETTY_FUNCTION__, error, [error userInfo]);
             abort();
         }
-        [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
     }
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [self.group.managedObjectContext save:nil];
-    Device *device = self.group.devices[self.tableView.indexPathForSelectedRow.row];
+    Device *device = [self deviceForIndexPath:indexPath];
     DeviceViewController *destinationViewController = [[DeviceViewController alloc] initWithStyle:UITableViewStyleGrouped];
     destinationViewController.device = device;
     [self.navigationController pushViewController:destinationViewController animated:YES];
